4798c07ba3f43df396d1d7b0657c298d739c07a2
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-06 23:05:32 +0000

    To string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Returns string representation of the argument
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns string representation
+    """
+    return str(n)
