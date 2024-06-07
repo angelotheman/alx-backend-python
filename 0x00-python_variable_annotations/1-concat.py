912b15bea2505b20b74e6df8db901ab0f3f558a3
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-07 11:30:10 +0000

    Updated

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Concatenate the string
 """
@@ -8,4 +8,4 @@ def concat(str1: str, str2: str) -> str:
     """
     Returns concatenated string
     """
-    returns str1 + str2
+    return str1 + str2
