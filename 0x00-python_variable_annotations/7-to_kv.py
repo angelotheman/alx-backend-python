66d621caf6c6263b3b2fa5082213afc454bf59ce
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-07 12:00:12 +0000

    to kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,5 +5,5 @@ Complex types string int and float
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: List[Union[int, float]]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     return (k, v ** 2)
