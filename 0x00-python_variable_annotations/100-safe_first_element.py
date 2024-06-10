19d6717c494f09bfb3bc3d5f6b8fe8df5c2b78ef
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-10 13:18:59 +0000

    Safe first element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Still on annotations
+"""
+from typing import Any, Union, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Safe first element
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
