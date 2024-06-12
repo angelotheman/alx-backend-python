d618e87889275191c2210afe324d56eb89100bfe
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-12 04:44:29 +0000

    Basic async syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Trying out async
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Determine the max delay
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
