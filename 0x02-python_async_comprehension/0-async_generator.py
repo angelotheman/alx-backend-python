cebc32b74311eba7077fe09be198c070aca118cb
Kwabena Twumasi <ktwum721@gmail.com>
2024-06-12 05:06:13 +0000

    Generator worked

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Test the generator function
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
