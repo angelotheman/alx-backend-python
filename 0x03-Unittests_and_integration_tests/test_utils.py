c730ab4ceb3c7366dea396be225a58bb302fb7de
Kwabena Twumasi <ktwum721@gmail.com>
2024-07-16 16:47:27 +0000

    ✅ Updated

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -75,40 +75,22 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """
-    Tests the `memoize` function.
-    """
-    class TestClass:
-        """
-        Class to test Memoize
-        """
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
 
-        def a_method(self):
-            """
-            A simple method that returns 42
-            """
-            return 42
-
-        @memoize
-        def a_property(self):
-            """
-            A memoized property that calls a_method
-            """
-            return self.a_method()
-
-        def test_memoize(self):
-            """
-            Test momoized decorator
-            """
-            test_instance = self.TestClass()
-
-            with patch.object(
-                    self.TestClass, 'a_method', return_value=42
-            ) as mock_method:
-                result1 = test_instance.a_property
-                result2 = test_instance.a_property
-
-                self.assertEqual(result1, 42)
-                self.assertEqual(result2, 42)
-
-                mock_method.assert_called_once()
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
